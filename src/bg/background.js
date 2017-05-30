1aa3922e72bb07a9861afb821f961984cc93c773
ujjawal mishra <ugomishra@gmail.com>
2017-05-30 22:03:37 +0530

    testing functionality

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,8 +1,8 @@
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+/*  var settings = new Store("settings", {
+    "sample_setting": "This is how you use Store.js to remember values"
+}); */
 
 
 //example of using a message handler from the inject scripts
@@ -11,3 +11,21 @@ chrome.extension.onMessage.addListener(
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+  
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "open_new_tab" ) {
+      //chrome.tabs.create({"url": request.url});
+    }
+  }
+);
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,7 +7,21 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
-
+		var firstHref = $("a[href^='http']").eq(0).attr("href");
+		chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
 	}
 	}, 10);
 });
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+	  
+	console.log("message se nt");
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
